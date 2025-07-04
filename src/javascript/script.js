fd5dcc05e658d3de89eb4d639275c937b6ac7502
Ana Berigo <ana.berigo17@gmail.com>
2025-07-04 08:57:16 -0300

    feat: Responsividade section AboutUs

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,3 +4,39 @@ $(document).ready(function () {
     $("#mobile_btn").find("i").toggleClass("fa-x");
   });
 });
+
+$(document).ready(function () {
+  let currentIndex = 0;
+  const $track = $(".carousel-track");
+  const $items = $(".carousel-item");
+  const totalItems = $items.length;
+
+  function isMobile() {
+    return $(window).width() <= 870;
+  }
+
+  function updateCarousel() {
+    if (isMobile()) {
+      const translateX = -currentIndex * 100;
+      $track.css("transform", `translateX(${translateX}%)`);
+    }
+  }
+
+  $(".next-btn").click(function () {
+    if (isMobile() && currentIndex < totalItems - 1) {
+      currentIndex++;
+      updateCarousel();
+    }
+  });
+
+  $(".prev-btn").click(function () {
+    if (isMobile() && currentIndex > 0) {
+      currentIndex--;
+      updateCarousel();
+    }
+  });
+
+  $(window).on("resize", function () {
+    updateCarousel();
+  });
+});
